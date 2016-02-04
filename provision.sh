bc00b986509649f50d81af640e526f9e703e85bc
Harm Müller <harm.mueller@inovex.de>
2016-02-04 22:06:40 +0100

    updated to 1.3.3_01 aka 1.4.0_rc1 an removed custom build

--- provision.sh
+++ provision.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 puppet apply --modulepath=/vagrant/modules/ /vagrant/manifests/default.pp
-wget -q https://github.com/RexOps/Rex/archive/arch.zip
-tmp_md5=$(md5sum arch.zip | awk '{ print $1 }')
-tmp_sha512=$(sha512sum arch.zip | awk '{ print $1 }')
-sed -ir "s/md5sums=.*/md5sums=(\'$tmp_md5\')/" /home/vagrant/PKGBUILD
-sed -ri "s/sha512sums=.*/sha512sums=(\'$tmp_sha512\')/" /home/vagrant/PKGBUILD
-su - vagrant -c "makepkg"
-pacman --noconfirm -U perl-rex-1.3.3-3-any.pkg.tar.xz 
-#rex test_rex
